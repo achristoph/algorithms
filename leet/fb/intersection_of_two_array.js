c3dfd46cd6dd49620370bdace9363abd04236f58
Arthur Christoph <achristoph@gmail.com>
2018-05-31 02:28:22 -0500

    .

--- leet/fb/intersection_of_two_array.js
+++ leet/fb/intersection_of_two_array.js
@@ -19,9 +19,30 @@ function intersect(nums1, nums2) {
   return arr;
 }
 
+//What if the given array is already sorted? How would you optimize your algorithm?
 function intersectSorted(nums1, nums2) {
   nums1.sort((a, b) => a - b);
   nums2.sort((a, b) => a - b);
+  let i = 0,
+    j = 0;
+  res = [];
+
+  while (i < nums1.length || j < nums2.length) {
+    if (nums1[i] > nums2[j]) {
+      j++;
+    } else if (nums1[i] < nums2[j]) {
+      i++;
+    } else {
+      // equal
+      res.push(nums1[i]);
+      i++;
+      j++;
+    }
+  }
+
+  return res;
 }
+
+//What if nums1's size is small compared to nums2's size? Which algorithm is better?
 
 console.log(intersectSorted([1, 2, 2, 1], [2, 2]));
