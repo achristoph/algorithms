343f9f5926278867e25627edc7bfc344edf39f26
Arthur Christoph <arthur.christoph@gmail.com>
2018-06-07 00:40:07 -0500

    .

--- leet/59.js
+++ leet/59.js
@@ -1,4 +1,4 @@
-function spiralOrder(matrix) {
+function spiralOrder1(matrix) {
   let ans = [];
   if (matrix.length == 0) return ans;
   let R = matrix.length,
@@ -21,6 +21,7 @@ function spiralOrder(matrix) {
       r = cr;
       c = cc;
     } else {
+      //hits a boundary, then switch direction by rotating dr and dc arrays
       di = (di + 1) % 4;
       r += dr[di];
       c += dc[di];
@@ -28,9 +29,26 @@ function spiralOrder(matrix) {
   }
   return ans;
 }
-console.log(spiralOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
 
-// let a = Array(3);
-// for (let i = 0; i < a.length; i++) {
-//   a[i] = [];
-// }
+function spiralOrder(matrix) {
+  ans = [];
+  if (matrix.length == 0) return ans;
+  let r1 = 0,
+    r2 = matrix.length - 1;
+  let c1 = 0,
+    c2 = matrix[0].length - 1;
+  while (r1 <= r2 && c1 <= c2) {
+    for (let c = c1; c <= c2; c++) ans.push(matrix[r1][c]);
+    for (let r = r1 + 1; r <= r2; r++) ans.push(matrix[r][c2]);
+    if (r1 < r2 && c1 < c2) {
+      for (let c = c2 - 1; c > c1; c--) ans.push(matrix[r2][c]);
+      for (let r = r2; r > r1; r--) ans.push(matrix[r][c1]);
+    }
+    r1++;
+    r2--;
+    c1++;
+    c2--;
+  }
+  return ans;
+}
+console.log(spiralOrder([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]));
